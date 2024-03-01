06b403c8eac498642e07637bdbd65d23a6cf4267
war-in <marcin.warchol20@gmail.com>
2024-03-01 14:48:58 +0100

    add with details test

--- crates/cairo-profiler/tests/e2e.rs
+++ crates/cairo-profiler/tests/e2e.rs
@@ -1,27 +1,6 @@
 use assert_fs::fixture::PathCopy;
 use snapbox::cmd::{cargo_bin, Command as SnapboxCommand};
-
-#[test]
-fn simple_package() {
-    let project_root = project_root::get_project_root().unwrap();
-    let temp_dir = assert_fs::TempDir::new().unwrap();
-    temp_dir
-        .copy_from(
-            project_root.join("crates/cairo-profiler/tests/data/"),
-            &["trace.json"],
-        )
-        .unwrap();
-
-    SnapboxCommand::new(cargo_bin!("cairo-profiler"))
-        .current_dir(&temp_dir)
-        .arg("./trace.json")
-        .assert()
-        .success();
-
-    assert!(temp_dir.join("profile.pb.gz").exists());
-
-    // TODO run pprof here
-}
+use test_case::test_case;
 
 #[test]
 fn output_path() {
@@ -42,4 +21,27 @@ fn output_path() {
         .success();
 
     assert!(temp_dir.join("my/output/dir/my_file.pb.gz").exists());
+}
+
+#[test_case(&["trace.json", "--show-details"]; "with details")]
+#[test_case(&["trace.json"]; "without details")]
+fn simple_package(args: &[&str]) {
+    let project_root = project_root::get_project_root().unwrap();
+    let temp_dir = assert_fs::TempDir::new().unwrap();
+    temp_dir
+        .copy_from(
+            project_root.join("crates/cairo-profiler/tests/data/"),
+            &["trace.json"],
+        )
+        .unwrap();
+
+    SnapboxCommand::new(cargo_bin!("cairo-profiler"))
+        .current_dir(&temp_dir)
+        .args(args)
+        .assert()
+        .success();
+
+    assert!(temp_dir.join("profile.pb.gz").exists());
+
+    // TODO run pprof here
 }
