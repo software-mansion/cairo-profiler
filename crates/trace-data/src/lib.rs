5dd95e770a04f918cc861bd79181f5816ff93a52
Marcin Warchoł <61014013+war-in@users.noreply.github.com>
2024-03-08 13:02:55 +0100

    Revert `class_hash` and `vm_trace` fields (#49)

--- crates/trace-data/src/lib.rs
+++ crates/trace-data/src/lib.rs
@@ -19,14 +19,6 @@ pub struct CallTrace {
     pub cumulative_resources: ExecutionResources,
     pub used_l1_resources: L1Resources,
     pub nested_calls: Vec<CallTrace>,
-    pub vm_trace: Vec<TraceEntry>,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct TraceEntry {
-    pub pc: usize,
-    pub ap: usize,
-    pub fp: usize,
 }
 
 #[derive(Debug, Default, Clone, Deserialize, Serialize)]
@@ -124,7 +116,6 @@ pub enum DeprecatedSyscallSelector {
 
 #[derive(Clone, Debug, Default, Eq, PartialEq, Serialize, Deserialize)]
 pub struct CallEntryPoint {
-    pub class_hash: ClassHash,
     pub entry_point_type: EntryPointType,
     pub entry_point_selector: EntryPointSelector,
     pub contract_address: ContractAddress,
