1d99cd07940402e858cc69b5be5729bae0564483
Piotr Magiera <56825108+piotmag769@users.noreply.github.com>
2024-03-08 11:10:20 +0100

    Add class hash (#47)

--- crates/trace-data/src/lib.rs
+++ crates/trace-data/src/lib.rs
@@ -124,6 +124,7 @@ pub enum DeprecatedSyscallSelector {
 
 #[derive(Clone, Debug, Default, Eq, PartialEq, Serialize, Deserialize)]
 pub struct CallEntryPoint {
+    pub class_hash: ClassHash,
     pub entry_point_type: EntryPointType,
     pub entry_point_selector: EntryPointSelector,
     pub contract_address: ContractAddress,
